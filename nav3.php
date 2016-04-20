549c89fab0abfa80f21a417f42ae82083facf924
Mary Scahill <info@maryscahill.net>
2016-04-20 04:09:17 -0400

    added file called nav3 where i'm planning additional nav buttons

---
+++ nav3.php
@@ -0,0 +1,49 @@
+<!doctype html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <!--    <nav class="navbar">-->
+    <div class="navContainer">
+        <div id="myNav">
+
+            <div class="row">
+                <div class="col-md-12">
+
+
+                    <a href="index.php">
+                        <button type=input class=mybtn style="width:100%;">
+                            MARY SCAHILL DOT NET </button>
+                    </a>
+
+
+                </div>
+            </div>
+            <div class="navItems">
+
+                    <a href="bio.php">
+                        <button id="bio" type=input class=mybtn> BIO
+                        </button>
+                    </a>
+
+                    <a href="cv.php">
+                        <button id="cv" type=input class=mybtn> CV
+                        </button>
+                    </a>
+
+                    <a href="contact.php">
+                        <button id="contact" type=input class=mybtn> CONTACT
+                        </button>
+                    </a>
+
+            </div>
+
+        </div>
+    </div>
+</body>
+
+</html>
