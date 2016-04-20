a202e55d83226d7d9942128bf3394f1b21132c74
Mary Scahill <info@maryscahill.net>
2016-04-20 02:00:58 -0400

    updated site description

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="MARY SCAHILL">
+    <meta name="description" content="(╯°□°）╯︵ ┻━┻ the best search results are gleaned by using natural language ┬──┬◡ﾉ(° -°ﾉ)">
     <meta name="author" content="MARY SCAHILL">
 
     <title>MARY SCAHILL DOT NET</title>
