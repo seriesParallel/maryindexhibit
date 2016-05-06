8c7df91c0787527083a6c6bfb0718d887a45b346
Mary Scahill <info@maryscahill.net>
2016-05-06 00:21:58 -0400

    added links to cv stuff

--- cv.php
+++ cv.php
@@ -9,14 +9,14 @@ Area of Study: Computer Science
 
 <p class="subject">Exhibitions</p>
 <p class="cvbody">
-2016 Pump Fake Superchief SOHO Gallery, NYC</br>
-2014 Death by Art, Death By Audio, Brooklyn, NY</br>
-Not Impossible Benefit Show, Wallspace Gallery, NYC
+2016 <a href="http://superchiefgallery.com/">Pump Fake Superchief SOHO Gallery, NYC</a></br>
+2014 <a href="http://www.ripdba.com/">Death by Art, Death By Audio, Brooklyn, NY</a></br>
+<a href="http://www.burke.org/media/news/2014/03/make-art-possible-support-the-brainwriter-benefit/191">Not Impossible Benefit Show, Wallspace Gallery, NYC</a>
 </p>
 
 <p class="subject">Web</p>
 <p class="cvbody">
-Mary Scahill  |  Pump Fake Interactive Environment  |  Computers Club Drawing Society  |  Asciifriends.net  |  Recode Project  |  Labour of Hate
+    <a href="http://www.maryscahill.net">Mary Scahill</a>  |  <a href="http://www.maryscahill.net/vyle/">Pump Fake Interactive Environment</a>  |  <a href="http://www.computersclub.org/draw/index.php?a_match=Mary+Scahill">Computers Club Drawing Society</a>  |  <a href="http://www.asciifriends.com/whois.html">Asciifriends.net</a>  |  <a href="http://recodeproject.com/artwork/v1n2kubus-serie">Recode Project</a>  |  <a href="https://twitter.com/labourofh8">Labour of Hate</a>
 </p>
 
 <p class="subject">Presentation</p>
@@ -32,14 +32,15 @@ Guest Lecture, Creative Computing, Parsons The New School, NYC
 
 <p class="subject">Select Professional</p>
 <p class="cvbody">
-2015 Fabrication: Anicka Yi, Maybe She’s Born with it, Brooklyn, NY</br>
-Live Visuals, Chromat AW15 Afterparty, Brooklyn, NY</br>
-Technical Engineering: Stephanie Rothenberg, Desiring Subjects Desiring Plants, NYC</br>
+2015 Fabrication: <a href="http://47canal.us/main.php?1=ay&amp;2=pics">Anicka Yi, Maybe She’s Born with it, Brooklyn, NY</a></br>
+<a href="http://chromat.co/blogs/news/18828331-chromat-aw15-after-party">Live Visuals, Chromat AW15 Afterparty, Brooklyn, NY</a></br>
+Technical Engineering: <a href="http://www.pan-o-matic.com/projects/planthropy">Stephanie Rothenberg, Desiring Subjects Desiring Plants, NYC</a></br>
 2014 Consultation: Louis Vuitton, Interactive Installation Design, NYC</br>
-Programming and Design: OpenBCI, 2014, Brooklyn, NY Creative Technologist, Fake Love, NYC
+Programming and Design: <a href="http://www.openbci.com/">OpenBCI Brooklyn, NY</a></br>
+Creative Technologist: <a href="http://fakelove.tv/">Fake Love, NYC</a>
 </p>
 
 <p class="subject">Press</p>
 <p class="cvbody">
-New Hive  |  Dis  |  Paper Mag
+    <a href="http://blog.newhive.com/vyle-releases-single-pump-fake-on-newhive/">New Hive</a>  |  <a href="http://dismagazine.com/blog/79725/premiere-pump-fake-by-vyle/">Dis</a>  |  <a href="http://www.papermag.com/inside-death-by-art-1427448705.html">Paper Mag</a>
 </p>
